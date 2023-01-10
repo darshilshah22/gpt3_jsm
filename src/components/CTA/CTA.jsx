112306ec45b5f801b3b8ef76ab389cbba546c7ee
Darshil <darshil@zealousweb.com>
2023-01-10 17:01:08 +0530

    project completed

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -3,7 +3,15 @@ import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="gpt3_cta">
+      <div className="gpt3_cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3_cta-btn">
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
